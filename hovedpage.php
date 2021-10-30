718bdd5db5a58ec7a482d9590c4837904a944da8
Marcus <89188899+MarcusKildal@users.noreply.github.com>
2021-10-30 16:43:21 +0200

    endret masse på koden
    jeg endret en del på koden å endret ide vi valgte at man skulle skrive spørsmål selv å man tryker på det man mener de er enige i

--- hovedpage.php
+++ hovedpage.php
@@ -1,7 +1,32 @@
+<?php
+$tjener = "localhost";
+$brukernavn = "root";
+$passord = "root";
+$database = "intilitypros"; //Endre på denne til din database
+
+// Opprette en kobling
+$kobling = new mysqli($tjener, $brukernavn, $passord, $database);
+
+// Sjekk om koblingen virker
+if($kobling->connect_error) {
+    die("Noe gikk galt: " . $kobling->connect_error);
+} else {
+    // echo "<br>";
+}
+// Angi UTF-8 som tegnsett
+$kobling->set_charset("utf8");
+
+
+if (isset($_GET["idrom"])){
+    $idrom = $_GET["idrom"];
+} else {
+    $idrom = 1;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="hovedpage.css">
+    <link rel="stylesheet" href="main.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,64 +34,92 @@
 
 </head>
 <body>
-    <h1>test</h1>
-<form action="kobling.php" method="get">
-     Super kult
-    <input type="radio" name="knappnavn" value ="Super kult">
-     Liker litt
-    <input type="radio" name="knappnavn" value ="Liker litt">
-     Usikker
-    <input type="radio" name="knappnavn" value ="Usikker">
-     Liker ikke 
-    <input type="radio" name="knappnavn" value ="Liker ikke">
-     Hater det 
-    <input type="radio" name="knappnavn" value ="Hater det">
-    <select name= "idrom">
-    <?php
-                    $tjener = "localhost";
-                    $brukernavn = "root";
-                    $passord = "root";
-                    $database = "intilitypros"; //Endre på denne til din database
+    <div>
+        <form action="kobling.php"method="get">
+            <input type="text" name="navn" placeholder="rom navn" required>
+            <input type="text" name="titel" placeholder="titel" required>
+            <input type="text" name="sporsmaal" placeholder="spørsmål" required>
+            <button type="submit">leg til rom</button>
+            <input type="hidden" name="idrom" value="<?php echo "$idrom";?>">   
+        </form>
+    </div>
+    <?php 
 
-                    // Opprette en kobling
-                    $kobling = new mysqli($tjener, $brukernavn, $passord, $database);
 
-                    // Sjekk om koblingen virker
-                    if($kobling->connect_error) {
-                        die("Noe gikk galt: " . $kobling->connect_error);
-                    } else {
-                        echo "<br>";
-                    }
+    $sql = "SELECT * FROM rom WHERE idrom = $idrom"; //Skriv din sql kode her
+    $resultat = $kobling->query($sql);
 
-                    // Angi UTF-8 som tegnsett
-                    $kobling->set_charset("utf8");
-                    // Med linjeskift for 1 tabell    
-                    $sql = "SELECT * FROM rom"; //Skriv din sql kode her
-                    $resultat = $kobling->query($sql);
-    
-                    while($rad = $resultat->fetch_assoc()) {
-                        $navn = $rad["navn"]; //Skriv ditt kolonnenavn her
-                        $idrom = $rad["idrom"];
-    
-                        echo "<option value='$idrom'> $navn</option>";
-                    }
+    while($rad = $resultat->fetch_assoc()) {
+        $navn = $rad["navn"]; //Skriv ditt kolonnenavn her
+        $titel = $rad["titel"]; //Skriv ditt kolonnenavn her
+        $sporsmaal = $rad["sporsmaal"]; //Skriv ditt kolonnenavn her
+
+        echo " <h1>$titel</h1>";
+        echo " <p>$sporsmaal<p>";
+        
+    }
     ?>
-    </select>
-    <input type="submit" value="Send">
+    <form action=""method="get">
+        <select name= "idrom">
+        <?php
+            // Med linjeskift for 1 tabell    
+            $sql = "SELECT * FROM rom"; //Skriv din sql kode her
+            $resultat = $kobling->query($sql);
 
-</form>
-<?php
+            while($rad = $resultat->fetch_assoc()) {
+                $navn = $rad["navn"]; //Skriv ditt kolonnenavn her
+                $idromdatabase = $rad["idrom"];
 
-// Med linjeskift for 1 tabell
-$sql = "SELECT * FROM Reaksjoner"; //Skriv din sql kode her
-$resultat = $kobling->query($sql);
+                if($idrom == $idromdatabase){
+                    echo "<option value='$idromdatabase' selected> $navn</option>";
+                } else {
+                    echo "<option value='$idromdatabase'> $navn</option>";
+                }
+                
+            }
+        ?>
+        <input type="submit" value="Velg rom">
+        </select>
+        </form>
 
-while($rad = $resultat->fetch_assoc()) {
-    $tid = $rad["Tid"]; //Skriv ditt kolonnenavn her
-    $navn = $rad["knappnavn"]; //Skriv ditt kolonnenavn her
 
-    echo "($navn) ble trykket, klokken ($tid) av (navn)  <br>";
-}
-?>
+    <form action="kobling.php"method="get">
+    <input type="text" name="brukernavn" placeholder="Username" required>
+        <br>
+        <input type="radio" name="knappnavn" value ="Super kult">
+        Super kult
+        <br>
+        <input type="radio" name="knappnavn" value ="Liker litt">
+        Liker litt
+        <br>
+        <input type="radio" name="knappnavn" value ="Usikker">
+        Usikker
+        <br>
+        <input type="radio" name="knappnavn" value ="Liker ikke">
+        Liker ikke 
+        <br>
+        <input type="radio" name="knappnavn" value ="Hater det">
+        Hater det 
+        <br>
+        <input type="submit" value="Send">
+        <input type="hidden" name="idrom" value="<?php echo "$idrom";?>">
+
+    </form>
+
+
+    <?php
+    // Med linjeskift for 1 tabell 
+    //JOIN rom ON WHERE idrom = 13"
+    $sql = "SELECT * FROM Reaksjoner JOIN rom ON idrom = rom_idrom WHERE idrom = $idrom"; //Skriv din sql kode her
+    $resultat = $kobling->query($sql);
+
+    while($rad = $resultat->fetch_assoc()) {
+        $tid = $rad["Tid"]; //Skriv ditt kolonnenavn her
+        $navn = $rad["knappnavn"]; //Skriv ditt kolonnenavn her
+        $brukernavnet = $rad["brukernavn"]; //Skriv ditt kolonnenavn her
+
+        echo "($navn) ble trykket, klokken ($tid) av ($brukernavnet)  <br>";
+    }
+    ?>
 </body>
 </html>
--- kobling.php
+++ kobling.php
@@ -14,27 +14,48 @@ $tjener = "localhost";
     } else {
         echo "Koblingen virker.<br>";
     }
-
     // Angi UTF-8 som tegnsett
     $kobling->set_charset("utf8");
 
+
+
+    
     $knappnavn = $_GET["knappnavn"];
-
     $rom = $_GET["idrom"];
-
-
+    $brukernavnet = $_GET["brukernavn"];
     date_default_timezone_set("Europe/Oslo");
     $når_knappen_ble_trykket = date("Y-m-d h:i:s");
 
 
+    if(isset($brukernavnet)){
+        $sql = "INSERT INTO `Reaksjoner` (`knappnavn`,`Tid`,`brukernavn`, `rom_idrom`) VALUES ('$knappnavn', '$når_knappen_ble_trykket', '$brukernavnet', '$rom')";
 
-    $sql = "INSERT INTO `Reaksjoner` (`knappnavn`,`Tid`,`rom_idrom`) VALUES ('$knappnavn', '$når_knappen_ble_trykket', '$rom')";
-
-    if($kobling->query($sql)) {
-        echo "Spørringen $sql ble gjennomført.";
-    } else {
-        echo "Noe gikk galt med spøfrringen $sql ($kobling->error).";
+        if($kobling->query($sql)) {
+            echo "Spørringen $sql ble gjennomført.";
+        } else {
+            echo "Noe gikk galt med spøfrringen $sql ($kobling->error).";
+        }
+        
     }
-    header("Location: hovedpage.php");
 
+
+   
+
+    $navn = $_GET["navn"];
+    $titel = $_GET["titel"];
+    $sporsmaal = $_GET["sporsmaal"];
+
+
+    if(isset($navn)){
+        $sql = "INSERT INTO `rom` (`navn`,`titel`,`sporsmaal`) VALUES ('$navn', '$titel', '$sporsmaal')";
+
+        if($kobling->query($sql)) {
+            echo "Spørringen $sql ble gjennomført.";
+        } else {
+            echo "Noe gikk galt med spøfrringen $sql ($kobling->error).";
+        }
+    
+    }
+
+    header("Location: hovedpage.php?idrom=$rom");
 ?>
